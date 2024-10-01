77c8cc3432a0b627fefd3e91c437f5e9a9a10473
wwaswa <williamwaswa2@gmail.com>
2024-10-01 17:00:34 +0000

    addedexecutablefile

---
+++ exec.sh
@@ -0,0 +1,7 @@
+##!binbash
+## written by Creven Waswa
+## This is an executable file to push github files
+##10-01-24
+git remote add origin https://github.com/wwaswa/etc.git
+git branch -M main
+git push -u origin main
